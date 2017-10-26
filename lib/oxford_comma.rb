49731dcfd05b376e6baeeb6bfade59297b0a3114
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-26 03:16:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,5 @@ def oxford_comma(array)
 #  if array.length == 2
 #    array.join(" and ")
 #  else
-    array.to_sentence(:last_word_connector => " and ")
-
+  [array[0...-1].join(", "), list.last].join(", and ")
 end
