5549e5997a9fe41e2bfe50eae93c2f463da6ac9f
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-26 03:16:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,5 @@ def oxford_comma(array)
 #  if array.length == 2
 #    array.join(" and ")
 #  else
-  [array[0...-1].join(", "), list.last].join(", and ")
+  [array[0...-1].join(", "), array.last].join(", and ")
 end
